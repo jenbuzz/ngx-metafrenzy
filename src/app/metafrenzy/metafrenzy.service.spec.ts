93ac76b38ddccd06b54d81e1ac23c50d3d274236
dan-lyn <daniel_lynge@hotmail.com>
2018-08-08 21:23:06 +0200

    added initial test of service class

---
+++ src/app/metafrenzy/metafrenzy.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { MetafrenzyService } from './metafrenzy.service';
+
+describe('MetafrenzyService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [MetafrenzyService]
+        });
+        this.service = TestBed.get(MetafrenzyService);
+    });
+   
+    it('should set and get title', () => {
+        const title = 'Lorem ipsum';
+        this.service.setTitle(title);
+        expect(this.service.getTitle()).toBe(title);
+    });
+
+    it('should set and get meta tag', () => {
+        const name = 'test';
+        const content = 'Lorem ipsum';
+        this.service.setMetaTag(name, content);
+        expect(this.service.getMetaTag('name=' + name)).toBe(content);
+    });
+});
