b6f6c5d070f03fa6617b19cfe5c8ac77fca9ff86
jenbuzz <daniel_lynge@hotmail.com>
2019-02-12 08:33:29 +0100

    updated test cases for settags

--- src/app/metafrenzy/metafrenzy.service.spec.ts
+++ src/app/metafrenzy/metafrenzy.service.spec.ts
@@ -65,4 +65,90 @@ describe('MetafrenzyService', () => {
 
         expect(this.service.getMetaTag('property="og:image:url"')).toBe(image);
     });
+
+    it('should set multiple tags except for title with one call', () => {
+        const description = 'Bla bla';
+        const url = 'http://test.test';
+        const image = 'image.jpg';
+        const robots = 'noindex,nofollow';
+
+        this.service.setTags({
+            description,
+            url,
+            image,
+            robots
+        });
+
+        expect(this.service.getMetaTag('name=description')).toBe(description);
+        expect(this.service.getMetaTag('property="og:description"')).toBe(description);
+
+        expect(this.service.getMetaTag('property="og:url"')).toBe(url);
+
+        expect(this.service.getMetaTag('property="og:image:url"')).toBe(image);
+    });
+
+    it('should set multiple tags except for description with one call', () => {
+        const title = 'Lorem ipsum';
+        const url = 'http://test.test';
+        const image = 'image.jpg';
+        const robots = 'noindex,nofollow';
+
+        this.service.setTags({
+            title,
+            url,
+            image,
+            robots
+        });
+
+        expect(this.service.getTitle()).toBe(title);
+        expect(this.service.getMetaTag('property="og:title"')).toBe(title);
+
+        expect(this.service.getMetaTag('property="og:url"')).toBe(url);
+
+        expect(this.service.getMetaTag('property="og:image:url"')).toBe(image);
+    });
+
+    it('should set multiple tags except for url with one call', () => {
+        const title = 'Lorem ipsum';
+        const description = 'Bla bla';
+        const image = 'image.jpg';
+        const robots = 'noindex,nofollow';
+
+        this.service.setTags({
+            title,
+            description,
+            image,
+            robots
+        });
+
+        expect(this.service.getTitle()).toBe(title);
+        expect(this.service.getMetaTag('property="og:title"')).toBe(title);
+
+        expect(this.service.getMetaTag('name=description')).toBe(description);
+        expect(this.service.getMetaTag('property="og:description"')).toBe(description);
+
+        expect(this.service.getMetaTag('property="og:image:url"')).toBe(image);
+    });
+
+    it('should set multiple tags except for image with one call', () => {
+        const title = 'Lorem ipsum';
+        const description = 'Bla bla';
+        const url = 'http://test.test';
+        const robots = 'noindex,nofollow';
+
+        this.service.setTags({
+            title,
+            description,
+            url,
+            robots
+        });
+
+        expect(this.service.getTitle()).toBe(title);
+        expect(this.service.getMetaTag('property="og:title"')).toBe(title);
+
+        expect(this.service.getMetaTag('name=description')).toBe(description);
+        expect(this.service.getMetaTag('property="og:description"')).toBe(description);
+
+        expect(this.service.getMetaTag('property="og:url"')).toBe(url);
+    });
 });
