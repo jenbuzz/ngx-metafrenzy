0b19a97762bb11b49bdc98b052a4d8830c091bd5
jenbuzz <daniel_lynge@hotmail.com>
2021-06-13 10:01:50 +0200

    update dependencies (angular 12)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,36 +12,36 @@ import { MetafrenzyModule, MetafrenzyGuard } from './metafrenzy';
         BrowserModule,
         MetafrenzyModule.forRoot(),
         RouterModule.forRoot([
-            {
-                path: '',
-                component: AppComponent
-            },
-            {
-                path: 'home',
-                component: AppComponent,
-                canActivate: [MetafrenzyGuard],
-                data: {
-                    metafrenzy: {
-                        title: 'Page 2',
-                        tags: [
-                            {
-                                name: 'og:title',
-                                content: 'Page 2'
-                            }, {
-                                name: 'og:description',
-                                content: 'Page description'
-                            }
-                        ],
-                        links: [
-                            {
-                                rel: 'canonical',
-                                href: 'http://localhost/home'
-                            }
-                        ]
+    {
+        path: '',
+        component: AppComponent
+    },
+    {
+        path: 'home',
+        component: AppComponent,
+        canActivate: [MetafrenzyGuard],
+        data: {
+            metafrenzy: {
+                title: 'Page 2',
+                tags: [
+                    {
+                        name: 'og:title',
+                        content: 'Page 2'
+                    }, {
+                        name: 'og:description',
+                        content: 'Page description'
                     }
-                }
+                ],
+                links: [
+                    {
+                        rel: 'canonical',
+                        href: 'http://localhost/home'
+                    }
+                ]
             }
-        ])
+        }
+    }
+], { relativeLinkResolution: 'legacy' })
     ],
     providers: [MetafrenzyGuard],
     bootstrap: [AppComponent]
